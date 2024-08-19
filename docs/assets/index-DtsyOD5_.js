d8fb20526bf50506764115019b0a898b7bc99244
imzbf <37092006+imzbf@users.noreply.github.com>
2024-08-19 08:45:03 +0000

    Deploying to docs from @ imzbf/md-editor-v3@5a5b2078b58811e27d5845ad8b41d47b811c61af 🚀

---
+++ docs/assets/index-DtsyOD5_.js
@@ -0,0 +1,35 @@
+import{d as o,a9 as n,a as d,w as r,aa as m,ah as a,ad as h,c}from"./index-mstXR5Qg.js";import{_ as l}from"./index.vue_vue_type_script_setup_true_lang-C8FZeH5G.js";import"./index-BABYOLP-.js";const i=`## About md-editor-v3
+
+<br>
+
+![](https://img.shields.io/github/stars/imzbf/md-editor-v3?style=social) ![](https://img.shields.io/npm/dm/md-editor-v3) ![](https://img.shields.io/bundlephobia/min/md-editor-v3) ![](https://img.shields.io/github/license/imzbf/md-editor-v3) ![](https://img.shields.io/github/package-json/v/imzbf/md-editor-v3) ![](https://img.shields.io/badge/ssr-%3E1.6.0-brightgreen)
+
+Markdown editor for vue3, developed in jsx and typescript, dark theme, beautify content by prettier, render articles directly, paste or clip the picture and upload it...
+
+## Same Series
+
+- For react，[md-editor-rt](https://github.com/imzbf/md-editor-rt)
+
+## Connect
+
+1. Email：zbfcqtl@gmail.com
+2. Github issue：[github issues](https://github.com/imzbf/md-editor-v3/issues)
+
+I'm not proficient in English, please help me correct wrong grammar.
+`,s=`## 关于 md-editor-v3
+
+<br>
+
+![](https://img.shields.io/github/stars/imzbf/md-editor-v3?style=social) ![](https://img.shields.io/npm/dm/md-editor-v3) ![](https://img.shields.io/bundlephobia/min/md-editor-v3) ![](https://img.shields.io/github/license/imzbf/md-editor-v3) ![](https://img.shields.io/github/package-json/v/imzbf/md-editor-v3) ![](https://img.shields.io/badge/ssr-%3E1.6.0-brightgreen)
+
+Markdown 编辑器，vue3 版本，使用 jsx 和 typescript 语法开发，支持切换主题，支持 prettier 美化文本，支持图片粘贴上传，裁剪上传，支持在 tsx 项目使用。
+
+## 同系列
+
+- react 版本，[md-editor-rt](https://github.com/imzbf/md-editor-rt)
+
+## 反馈联系
+
+1. 邮箱：zbfcqtl@gmail.com
+2. issue 管理：[github issues](https://github.com/imzbf/md-editor-v3/issues)
+`,p={class:"container"},u={class:"doc"},g={name:"AboutPage"},x=o({...g,setup(b){const t=n(),e=d(t.state.lang==="en-US"?i:s);return r(()=>t.state.lang,()=>{e.value=t.state.lang==="en-US"?i:s}),(v,_)=>(m(),a("div",p,[h("div",u,[c(l,{editorId:"md-about",modelValue:e.value},null,8,["modelValue"])])]))}});export{x as default};
